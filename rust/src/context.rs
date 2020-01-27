3c4aae993a149aa0fd14c0f4cf95978d3fe1d4d0
Alex <silvercircle@gmail.com>
2020-01-27 18:27:09 +0100

    add rust version #3
    Signed-off-by: Alex <silvercircle@gmail.com>

--- rust/src/context.rs
+++ rust/src/context.rs
@@ -1,59 +1,84 @@
 extern crate app_dirs;
-extern crate serde_json;
-extern crate serde;
+pub mod database;
 
 use std::sync::Once;
 use std::mem;
 use std::path::{PathBuf, Path};
-use std::fs;
 use app_dirs::*;
+use std::fs::{File, create_dir_all};
+use std::io::prelude::*;
+use simplelog::{WriteLogger, LevelFilter, Config as LogConfig};
 use serde::{Deserialize, Serialize};
-use serde_json::{Result};
 
 const APP_INFO: AppInfo = AppInfo{name: "darksky-r", author: "alex"};
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
-    pub _home_dir: String,
     pub _config_dir: PathBuf,
     pub _data_dir: PathBuf,
     pub _log_file_name: PathBuf,
+    pub _cache_file_name: PathBuf,
     pub _apikey: String,
-    pub _is_intialiazed: bool
+    pub _loc: String,
+    pub _is_intialiazed: bool,
+    pub _updated_from_api: u32,
+    pub _lastrun: String
 }
 
 pub struct Context {
     pub _cfg: Config,
     pub _use_count: i32,
-    pub _exe_path: PathBuf
+    pub _exe_path: PathBuf,
+    _db: database::DB
 }
 
 impl Context {
     pub fn init(&mut self) -> std::io::Result<()> {
-        fs::create_dir_all(&self._cfg._config_dir)?;
-        let mut config_file_name = self._cfg._config_dir.clone();
-        config_file_name.push("conf.json");
+        create_dir_all(&self._cfg._config_dir)?;
+        let config_file_name = self.get_config_filename();
+        let log_file_name = self.get_log_filename();
+        let _logresult = WriteLogger::init(LevelFilter::Info, LogConfig::default(), File::create(log_file_name.clone())?);
 
-        if Path::new(config_file_name.as_os_str()).exists() {
-            println!("The config file name is: {:?}", config_file_name.as_os_str());
+        log::info!("Context::init(): Log file created at {}", log_file_name);
+        debug_assert_ne!(config_file_name.len(), 0);
+
+        if Path::new(config_file_name.as_str()).exists() {
+            log::info!("Context::init(): Found a config file at {} and using it", config_file_name);
+            let mut _file = File::open(config_file_name)?;
+            let mut _raw_json: String = String::new();
+
+            _file.read_to_string(&mut _raw_json)?;
+            self._cfg = serde_json::from_str(_raw_json.as_str())?;
         } else {
-            let l = serde_json::to_string_pretty(&self._cfg).unwrap();
-            println!("{}", l);
+            log::info!("Context::init(): The config file did not exist. Creating at {}", config_file_name);
+            let _l = serde_json::to_string_pretty(&self._cfg).unwrap();
+            let mut _file = File::create(config_file_name)?;
+            _file.write_all(_l.as_bytes())?;
         }
-        println!("Singleton initialized in {:#?}", self._exe_path);
+
+        let mut database_filename = self._cfg._data_dir.clone();
+        database_filename.push("darksky-r.sqlite");
+        let _dbresult = self._db.connect(database_filename.to_str().unwrap());
+        log::info!("Context::init(): Created the database at {}", database_filename.to_str().unwrap());
         Ok(())
     }
 
     pub fn cleanup(&mut self) {
-        println!("Cleaning up Context");
+        let _r = self.write_config();
     }
 
     pub fn inc_use_count(&mut self) {
         self._use_count = self._use_count + 1;
     }
 
-    pub fn write_config(&self) {
-    
+    pub fn write_config(&self) -> std::io::Result<()> {
+        let _config_file_name = self.get_config_filename();
+        let mut _file = File::create(_config_file_name)?;
+        let _l = serde_json::to_string_pretty(&self._cfg).unwrap();
+        println!("JSON final:\n{}", _l);
+        _file.write_all(_l.as_bytes())?;
+        _file.flush()?;
+        Ok(())
     }
 
     pub fn get_config_filename(&self) -> String {
@@ -61,12 +86,21 @@ impl Context {
         config_file_name.push("conf.json");
         String::from(config_file_name.to_str().unwrap().clone())
     }
+
+    pub fn get_log_filename(&self) -> String {
+        let mut log_file_name = self._cfg._data_dir.clone();
+        log_file_name.push("darksky-r.log");
+        String::from(log_file_name.to_str().unwrap().clone())
+    }
 }
 
 /*
  * this creates exactly one instance of Context and returns a reference
  * to it.
- * it is a Singleton implementation
+ *
+ * It is very similar to the Singleton pattern in other languages and can be used
+ * for application-wide objects that only must exist once.
+ * https://stackoverflow.com/questions/27791532/how-do-i-create-a-global-mutable-singleton
  */
 
 pub fn get_instance() -> &'static mut Context {
@@ -74,18 +108,24 @@ pub fn get_instance() -> &'static mut Context {
     static ONCE: Once = Once::new();
     unsafe {
         ONCE.call_once(|| {
-            let singleton = Context {
+            let data_dir = get_app_dir(AppDataType::UserData, &APP_INFO, "").unwrap();
+            let context = Context {
                 _use_count: 0,
+                _db: database::DB::default(),
                 _exe_path: std::env::current_exe().unwrap(),
-                _cfg: Config { _home_dir: "Foo".to_string(),
+                _cfg: Config {
+                    _lastrun: chrono::Local::now().to_rfc3339(),
                     _is_intialiazed: false,
                     _apikey: "FOO".to_string(),
+                    _updated_from_api: 0,
+                    _loc: "0000".to_string(),
                     _config_dir: get_app_dir(AppDataType::UserConfig, &APP_INFO, "").unwrap(),
-                    _data_dir: get_app_dir(AppDataType::UserData, &APP_INFO, "").unwrap(),
-                    _log_file_name: [ get_app_dir(AppDataType::UserData, &APP_INFO, "").unwrap(), PathBuf::from("log.log") ].iter().collect()
+                    _data_dir: data_dir.clone(),
+                    _log_file_name: [ data_dir.clone(), PathBuf::from("log.log") ].iter().collect(),
+                    _cache_file_name: [ data_dir.clone(), PathBuf::from("cache.json") ].iter().collect()
                 }
             };
-            CTX = mem::transmute(Box::new(singleton));
+            CTX = mem::transmute(Box::new(context));
 
             let ctx = &mut *CTX;
             let _result = ctx.init();  // (*CTX).init(); does the same
@@ -95,5 +135,3 @@ pub fn get_instance() -> &'static mut Context {
         &mut*CTX
     }
 }
-
-
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,19 +1,140 @@
+/*
++ darksky-r fetches weather data from darksky.net and builds
++ a plain text file which conky will read to construct a nice weather
++ widget.
++ +
++ © 2020, Alex Vie <silvercircle@gmail.com>
++ License: MIT
++
++ How to build:
++
++ install a rust toolchain
++
++ > cargo build --release
+*/
+
+extern crate log;
+extern crate simplelog;
+extern crate clap;
+extern crate chrono;
+extern crate reqwest;
+extern crate serde_json;
+extern crate serde;
+#[macro_use]
+extern crate lazy_static;
 
 mod context;
+use chrono::{DateTime, Local};
+use std::fs::{File};
+use std::io::prelude::*;
+use std::sync::{Mutex};
 
+pub struct LocalConfig {
+    history:    bool,
+    tempunit:   char,
+    cached:     bool,
+    visunit:    String,
+    windunit:   String,
+    pressunit:  String,
+    running:    bool,
+}
+
+impl LocalConfig {
+    pub fn new() -> Self {
+        println!("Builing local config");
+        Self {
+            history:    true,
+            tempunit:   'C',
+            cached:     true,
+            visunit:    "km".to_string(),
+            windunit:   "ms".to_string(),
+            pressunit:  "hpa".to_string(),
+            running:    false,
+        }
+    }
+}
+
+// this makes a mutable global reference to an instance of LocalConfig
+// a Mutex is required to access it
+lazy_static! {
+    pub static ref CONFIG: Mutex<LocalConfig> = Mutex::new(LocalConfig::new());
+}
 fn main() {
-    println!("Hello, world!");
+    let ctx = context::get_instance();
+    // test global reference to LocalConfig object
+        {
+            let mut _c = CONFIG.lock().unwrap();
+            println!("Config.running initial value: {}", _c.running);
+            _c.running = true;
+            println!("Config.running is now: {}", _c.running);
+        }
+        let mut _c = CONFIG.lock().unwrap();
+        println!("Config.running is still: {}", _c.running);
+    //
+    let _matches = clap::App::new("darksky-r")
+        .version("0.1")
+        .arg(clap::Arg::with_name("apikey")
+            .short("a")
+            .long("apikey")
+            .value_name("APIKEY")
+            .help("Set the Api key"))
+        .arg(clap::Arg::with_name("loc")
+            .short("l")
+            .long("loc")
+            .value_name("LOC")
+            .help("Set your latitude/longitude, e.g. --loc=48.2082,16.3738"))
+        .arg(clap::Arg::with_name("no-history")
+            .short("n")
+            .long("no-history")
+            .value_name("NO-HISTORY")
+            .help("Do not record the data set in the database"))
+        .arg(clap::Arg::with_name("tempunit")
+            .short("t")
+            .long("tempunit")
+            .value_name("TEMPUNIT")
+            .help("Set the unit for temperature. Allowed are C or F, default is C"))
+        .arg(clap::Arg::with_name("visunit")
+            .short("v")
+            .long("visunit")
+            .value_name("VISUNIT")
+            .help("Set the unit for visibility. Allowed are 'km' or 'miles', default is 'km"))
+        .arg(clap::Arg::with_name("pressunit")
+            .short("p")
+            .long("pressunit")
+            .value_name("PRESSUNIT")
+            .help("Set the unit for pressure. Allowed are 'hpa' or 'inhg', default is 'km"))
+        .arg(clap::Arg::with_name("windunit")
+            .short("w")
+            .long("windunit")
+            .value_name("WINDUNIT")
+            .help("Set the unit for wind speed. Allowed are 'ms', 'km', 'knots' or 'mph' are allowed, defaults to 'ms"))
+        .arg(clap::Arg::with_name("cached")
+            .short("c")
+            .long("cached")
+            .value_name("CACHED")
+            .help("Use cached Json result, if there is none, fetch from API nonetheless."))
+        .get_matches();
 
-    let ctx: &context::Context = context::get_instance();
-    println!("Name is and I have been used {} times", ctx._use_count);
+    ctx._cfg._apikey = String::from(_matches.value_of("apikey").unwrap_or(ctx._cfg._apikey.as_str()));
+    ctx._cfg._loc = String::from(_matches.value_of("loc").unwrap_or(ctx._cfg._loc.as_str()));
+    let _foo: DateTime<Local> = Local::now();
+    println!("Last run time: {}", ctx._cfg._lastrun);
+    ctx._cfg._lastrun = _foo.to_rfc3339();
+    let _url = format!("https://api.darksky.net/forecast/{}/{}{}", ctx._cfg._apikey, ctx._cfg._loc, "?units=si");
+    log::info!("Requesting from: {}", _url);
+    let _rsp = fetch(&_url);
+    ctx.inc_use_count();
+    ctx.cleanup();
+}
 
-    let ctx1 = context::get_instance();
-    println!("Name is and I have been used {} times", ctx1._use_count);
-    ctx1.inc_use_count();
-    println!("Name is and I have been used {} times", ctx1._use_count);
+pub fn fetch(url: &String) -> reqwest::Result<String> {
+    let ctx = context::get_instance();
 
-    println!("Config dir: {:?}", ctx1._cfg._config_dir);
-    println!("Data dir:   {:?}", ctx1._cfg._data_dir);
-    println!("Log File:   {:?}", ctx1._cfg._log_file_name);
-    ctx1.cleanup();
+    let response = reqwest::blocking::get(url)?.text()?;
+    let _val: serde_json::Value = serde_json::json!(response);
+
+    let mut _file = File::create(ctx._cfg._cache_file_name.to_str().unwrap()).unwrap();
+    let _result = _file.write_all(response.as_bytes());
+
+    Ok(response)
 }
