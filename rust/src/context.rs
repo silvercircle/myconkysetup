a573a099c10390c597dac9755329c8a5902252f2
Alex <silvercircle@gmail.com>
2020-01-27 18:27:09 +0100

    add rust version #2

--- rust/src/context.rs
+++ rust/src/context.rs
@@ -2,13 +2,16 @@ extern crate app_dirs;
 extern crate serde_json;
 extern crate serde;
 
+pub mod database;
+
 use std::sync::Once;
 use std::mem;
 use std::path::{PathBuf, Path};
-use std::fs;
 use app_dirs::*;
+use std::fs::{File, create_dir_all};
+use std::io::prelude::*;
+use simplelog::{WriteLogger, LevelFilter, Config as LogConfig};
 use serde::{Deserialize, Serialize};
-use serde_json::{Result};
 
 const APP_INFO: AppInfo = AppInfo{name: "darksky-r", author: "alex"};
 
@@ -25,35 +28,56 @@ pub struct Config {
 pub struct Context {
     pub _cfg: Config,
     pub _use_count: i32,
-    pub _exe_path: PathBuf
+    pub _exe_path: PathBuf,
+    _db: database::DB
 }
 
 impl Context {
     pub fn init(&mut self) -> std::io::Result<()> {
-        fs::create_dir_all(&self._cfg._config_dir)?;
-        let mut config_file_name = self._cfg._config_dir.clone();
-        config_file_name.push("conf.json");
+        create_dir_all(&self._cfg._config_dir)?;
+        let config_file_name = self.get_config_filename();
+        let log_file_name = self.get_log_filename();
+        let _logresult = WriteLogger::init(LevelFilter::Info, LogConfig::default(), File::create(log_file_name.clone())?);
 
-        if Path::new(config_file_name.as_os_str()).exists() {
-            println!("The config file name is: {:?}", config_file_name.as_os_str());
+        log::info!("Context::init(): Log file created at {}", log_file_name);
+        debug_assert_ne!(config_file_name.len(), 0);
+
+        if Path::new(config_file_name.as_str()).exists() {
+            log::info!("Context::init(): Found a config file at {} and using it", config_file_name);
+            let mut _file = File::open(config_file_name)?;
+            let mut _raw_json: String = String::new();
+
+            _file.read_to_string(&mut _raw_json)?;
+            self._cfg = serde_json::from_str(_raw_json.as_str())?;
         } else {
-            let l = serde_json::to_string_pretty(&self._cfg).unwrap();
-            println!("{}", l);
+            log::info!("Context::init(): The config file did not exist. Creating at {}", config_file_name);
+            let _l = serde_json::to_string_pretty(&self._cfg).unwrap();
+            let mut _file = File::create(config_file_name)?;
+            _file.write_all(_l.as_bytes())?;
         }
-        println!("Singleton initialized in {:#?}", self._exe_path);
+
+        let mut database_filename = self._cfg._data_dir.clone();
+        database_filename.push("darksky-r.sqlite");
+        let _dbresult = self._db.connect(database_filename.to_str().unwrap());
+        log::info!("Context::init(): Created the database at {}", database_filename.to_str().unwrap());
         Ok(())
     }
 
     pub fn cleanup(&mut self) {
-        println!("Cleaning up Context");
+        let _r = self.write_config();
     }
 
     pub fn inc_use_count(&mut self) {
         self._use_count = self._use_count + 1;
     }
 
-    pub fn write_config(&self) {
-    
+    pub fn write_config(&self) -> std::io::Result<()> {
+        let _config_file_name = self.get_config_filename();
+        let mut _file = File::create(_config_file_name)?;
+        let _l = serde_json::to_string_pretty(&self._cfg).unwrap();
+        _file.write_all(_l.as_bytes())?;
+        _file.flush()?;
+        Ok(())
     }
 
     pub fn get_config_filename(&self) -> String {
@@ -61,12 +85,20 @@ impl Context {
         config_file_name.push("conf.json");
         String::from(config_file_name.to_str().unwrap().clone())
     }
+
+    pub fn get_log_filename(&self) -> String {
+        let mut log_file_name = self._cfg._data_dir.clone();
+        log_file_name.push("darksky-r.log");
+        String::from(log_file_name.to_str().unwrap().clone())
+    }
 }
 
 /*
  * this creates exactly one instance of Context and returns a reference
  * to it.
- * it is a Singleton implementation
+ *
+ * It is very similar to the Singleton pattern in other languages and can be used
+ * for application-wide objects that only must exist once.
  */
 
 pub fn get_instance() -> &'static mut Context {
@@ -76,6 +108,7 @@ pub fn get_instance() -> &'static mut Context {
         ONCE.call_once(|| {
             let singleton = Context {
                 _use_count: 0,
+                _db: database::DB::default(),
                 _exe_path: std::env::current_exe().unwrap(),
                 _cfg: Config { _home_dir: "Foo".to_string(),
                     _is_intialiazed: false,
---
+++ rust/src/context/database.rs
@@ -0,0 +1,45 @@
+extern crate rusqlite;
+
+use rusqlite::NO_PARAMS;
+
+pub struct DB {
+    _path: String,
+    _valid: bool,
+    _conn: Option<rusqlite::Connection>
+}
+
+impl Default for DB {
+    fn default() -> DB {
+        DB { _path: String::from(""), _valid: false, _conn: None }
+    }
+}
+
+impl DB {
+    pub fn connect(&mut self, _path: &str) -> rusqlite::Result<()> {
+        self._conn = Some(rusqlite::Connection::open(_path).unwrap());
+        let _c = self._conn.as_ref().unwrap();
+        let _dbresult = _c.execute(r#"CREATE TABLE IF NOT EXISTS history
+						(
+							id INTEGER PRIMARY KEY AUTOINCREMENT,
+							timestamp INTEGER DEFAULT 0,
+							summary TEXT NOT NULL DEFAULT 'unknown',
+							icon TEXT NOT NULL DEFAULT 'unknown',
+							temperature REAL NOT NULL DEFAULT 0.0,
+							feelslike REAL NOT NULL DEFAULT 0.0,
+							dewpoint REAL DEFAULT 0.0,
+							windbearing INTEGER DEFAULT 0,
+							windspeed REAL DEFAULT 0.0,
+							windgust REAL DEFAULT 0.0,
+							humidity REAL DEFAULT 0.0,
+							visibility REAL DEFAULT 0.0,
+							pressure REAL DEFAULT 1013.0,
+							precip_probability REAL DEFAULT 0.0,
+							precip_intensity REAL DEFAULT 0.07,
+							precip_type TEXT DEFAULT 'none',
+							uvindex INTEGER DEFAULT 0,
+							sunrise INTEGER DEFAULT 0,
+							sunset INTEGER DEFAULT 0
+						)"#, NO_PARAMS)?;
+        Ok(())
+    }
+}
