8b091e48c4470951112428b2d71af8b84adef0c9
Alex <silvercircle@gmail.com>
2023-03-03 22:20:16 +0100

    silence clippy

--- rust/src/context.rs
+++ rust/src/context.rs
@@ -14,122 +14,126 @@ use std::sync::Once;
 use handlebars::{handlebars_helper, Handlebars};
 
 const APP_INFO: AppInfo = AppInfo {
-    name: "darksky-r",
-    author: "alex",
+  name: "darksky-r",
+  author: "alex",
 };
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
-    pub _config_dir: PathBuf,
-    pub _data_dir: PathBuf,
-    pub _log_file_name: PathBuf,
-    pub _cache_file_name: PathBuf,
-    pub _apikey: String,
-    pub _loc: String,
-    pub _is_intialiazed: bool,
-    pub _updated_from_api: u32,
-    pub _lastrun: String,
+  pub _config_dir: PathBuf,
+  pub _data_dir: PathBuf,
+  pub _log_file_name: PathBuf,
+  pub _cache_file_name: PathBuf,
+  pub _apikey: String,
+  pub _loc: String,
+  pub _is_intialiazed: bool,
+  pub _updated_from_api: u32,
+  pub _lastrun: String,
 }
 
 pub struct Context {
-    pub _cfg: Config,
-    pub _use_count: i32,
-    pub _exe_path: PathBuf,
-    pub _db: database::DB,
-    pub _data: datahandler::DataHandler,
-    pub _json_valid: bool,
-    pub _templates: Handlebars<'static>,
+  pub _cfg: Config,
+  pub _use_count: i32,
+  pub _exe_path: PathBuf,
+  pub _db: database::DB,
+  pub _data: datahandler::DataHandler,
+  pub _json_valid: bool,
+  pub _templates: Handlebars<'static>,
 }
 
 impl Context {
-    pub fn init(&mut self) -> std::io::Result<()> {
-        create_dir_all(&self._cfg._config_dir)?;
-        let config_file_name = self.get_config_filename();
-        let log_file_name = self.get_log_filename();
-        let _logresult = WriteLogger::init(
-            LevelFilter::Info,
-            LogConfig::default(),
-            File::create(log_file_name.clone())?,
-        );
-        log::info!("Context::init(): Log file created at {}", log_file_name);
-        log::
-        debug_assert_ne!(config_file_name.len(), 0);
-        if Path::new(config_file_name.as_str()).exists() {
-            log::info!(
-                "Context::init(): Found a config file at {} and using it",
-                config_file_name
-            );
-            let mut _file = File::open(config_file_name)?;
-            let mut _raw_json: String = String::new();
+  pub fn init(&mut self) -> std::io::Result<()> {
+    create_dir_all(&self._cfg._config_dir)?;
+    let config_file_name = self.get_config_filename();
+    let log_file_name = self.get_log_filename();
+    let _logresult = WriteLogger::init(
+      LevelFilter::Info,
+      LogConfig::default(),
+      File::create(log_file_name.clone())?,
+    );
+    log::info!("Context::init(): Log file created at {}", log_file_name);
+    if Path::new(config_file_name.as_str()).exists() {
+      log::info!(
+        "Context::init(): Found a config file at {} and using it",
+        config_file_name
+      );
+      let mut _file = File::open(config_file_name)?;
+      let mut _raw_json: String = String::new();
 
-            _file.read_to_string(&mut _raw_json)?;
-            self._cfg = serde_json::from_str(_raw_json.as_str())?;
-        } else {
-            log::info!(
-                "Context::init(): The config file did not exist. Creating at {}",
-                config_file_name
-            );
-            let _l = serde_json::to_string_pretty(&self._cfg).unwrap();
-            let mut _file = File::create(config_file_name)?;
-            _file.write_all(_l.as_bytes())?;
-        }
-
-        let mut database_filename = self._cfg._data_dir.clone();
-        database_filename.push("history.sqlite3");
-        let _dbresult = self._db.connect(database_filename.to_str().unwrap());
-        log::info!(
-            "Context::init(): Created the database at {}",
-            database_filename.to_str().unwrap()
-        );
-
-        handlebars::handlebars_helper!(onedecimal: |v: f64| format!("{:.1}", v));
-        handlebars::handlebars_helper!(nodecimal: |v: f64| format!("{:.0}", v));
-
-        self._templates
-            .register_helper("onedecimal", Box::new(onedecimal));
-        self._templates
-            .register_helper("nodecimal", Box::new(nodecimal));
-        self._templates
-            .register_template_string("one_decimal_and_unit", "{{onedecimal value}}{{unit}}")
-            .unwrap();
-        self._templates
-            .register_template_string("zero_decimal_and_unit", "{{nodecimal value}}{{unit}}")
-            .unwrap();
-        self._templates
-            .register_template_string("one_decimal_no_unit", "{{onedecimal value}}")
-            .unwrap();
-        self._templates
-            .register_template_string("zero_decimal_no_unit", "{{nodecimal value}}")
-            .unwrap();
-
-        Ok(())
+      _file.read_to_string(&mut _raw_json)?;
+      self._cfg = serde_json::from_str(_raw_json.as_str())?;
+    } else {
+      log::info!(
+        "Context::init(): The config file did not exist. Creating at {}",
+        config_file_name
+      );
+      let _l = serde_json::to_string_pretty(&self._cfg).unwrap();
+      let mut _file = File::create(config_file_name)?;
+      _file.write_all(_l.as_bytes())?;
     }
 
-    pub fn cleanup(&mut self) {
-        self._db.close();
-        let _r = self.write_config();
-    }
+    let mut database_filename = self._cfg._data_dir.clone();
+    database_filename.push("history.sqlite3");
+    let _dbresult = self._db.connect(database_filename.to_str().unwrap());
+    log::info!(
+      "Context::init(): Created the database at {}",
+      database_filename.to_str().unwrap()
+    );
 
-    pub fn write_config(&self) -> std::io::Result<()> {
-        let _config_file_name = self.get_config_filename();
-        let mut _file = File::create(_config_file_name)?;
-        let _l = serde_json::to_string_pretty(&self._cfg).unwrap();
-        _file.write_all(_l.as_bytes())?;
-        _file.flush()?;
-        Ok(())
-    }
+    handlebars::handlebars_helper!(onedecimal: |v: f64| format!("{:.1}", v));
+    handlebars::handlebars_helper!(nodecimal: |v: f64| format!("{:.0}", v));
 
-    pub fn get_config_filename(&self) -> String {
-        let mut config_file_name = self._cfg._config_dir.clone();
-        config_file_name.push("conf.json");
-        String::from(config_file_name.to_str().unwrap().clone())
-    }
+    self
+      ._templates
+      .register_helper("onedecimal", Box::new(onedecimal));
+    self
+      ._templates
+      .register_helper("nodecimal", Box::new(nodecimal));
+    self
+      ._templates
+      .register_template_string("one_decimal_and_unit", "{{onedecimal value}}{{unit}}")
+      .unwrap();
+    self
+      ._templates
+      .register_template_string("zero_decimal_and_unit", "{{nodecimal value}}{{unit}}")
+      .unwrap();
+    self
+      ._templates
+      .register_template_string("one_decimal_no_unit", "{{onedecimal value}}")
+      .unwrap();
+    self
+      ._templates
+      .register_template_string("zero_decimal_no_unit", "{{nodecimal value}}")
+      .unwrap();
 
-    pub fn get_log_filename(&self) -> String {
-        let mut log_file_name = self._cfg._data_dir.clone();
-        log_file_name.push("darksky-r.log");
-        String::from(log_file_name.to_str().unwrap().clone())
-    }
+    Ok(())
+  }
+
+  pub fn cleanup(&mut self) {
+    self._db.close();
+    let _r = self.write_config();
+  }
+
+  pub fn write_config(&self) -> std::io::Result<()> {
+    let _config_file_name = self.get_config_filename();
+    let mut _file = File::create(_config_file_name)?;
+    let _l = serde_json::to_string_pretty(&self._cfg).unwrap();
+    _file.write_all(_l.as_bytes())?;
+    _file.flush()?;
+    Ok(())
+  }
+
+  pub fn get_config_filename(&self) -> String {
+    let mut config_file_name = self._cfg._config_dir.clone();
+    config_file_name.push("conf.json");
+    String::from(config_file_name.to_str().unwrap().clone())
+  }
+
+  pub fn get_log_filename(&self) -> String {
+    let mut log_file_name = self._cfg._data_dir.clone();
+    log_file_name.push("darksky-r.log");
+    String::from(log_file_name.to_str().unwrap().clone())
+  }
 }
 
 /*
@@ -145,40 +149,40 @@ impl Context {
  */
 
 pub fn get_instance() -> &'static mut Context {
-    static mut CTX: *mut Context = 0 as *mut Context;
-    static ONCE: Once = Once::new();
-    unsafe {
-        ONCE.call_once(|| {
-            let data_dir = get_app_dir(AppDataType::UserData, &APP_INFO, "").unwrap();
-            let context = Context {
-                _use_count: 0,
-                _db: database::DB::default(),
-                _exe_path: std::env::current_exe().unwrap(),
-                _data: datahandler::DataHandler::new(),
-                _json_valid: false,
-                _templates: Handlebars::new(),
-                _cfg: Config {
-                    _lastrun: chrono::Local::now().to_rfc3339(),
-                    _is_intialiazed: false,
-                    _apikey: "FOO".to_string(),
-                    _updated_from_api: 0,
-                    _loc: "0000".to_string(),
-                    _config_dir: get_app_dir(AppDataType::UserConfig, &APP_INFO, "").unwrap(),
-                    _data_dir: data_dir.clone(),
-                    _log_file_name: [data_dir.clone(), PathBuf::from("log.log")]
-                        .iter()
-                        .collect(),
-                    _cache_file_name: [data_dir.clone(), PathBuf::from("cache.json")]
-                        .iter()
-                        .collect(),
-                },
-            };
-            CTX = mem::transmute(Box::new(context));
-            let ctx = &mut *CTX;
-            let _result = ctx.init(); // (*CTX).init(); does the same
-            ctx._cfg._is_intialiazed = true;
-        });
-        (*CTX)._use_count = (*CTX)._use_count + 1;
-        &mut *CTX // return mutable reference to the object
-    }
+  static mut CTX: *mut Context = 0 as *mut Context;
+  static ONCE: Once = Once::new();
+  unsafe {
+    ONCE.call_once(|| {
+      let data_dir = get_app_dir(AppDataType::UserData, &APP_INFO, "").unwrap();
+      let context = Context {
+        _use_count: 0,
+        _db: database::DB::default(),
+        _exe_path: std::env::current_exe().unwrap(),
+        _data: datahandler::DataHandler::new(),
+        _json_valid: false,
+        _templates: Handlebars::new(),
+        _cfg: Config {
+          _lastrun: chrono::Local::now().to_rfc3339(),
+          _is_intialiazed: false,
+          _apikey: "FOO".to_string(),
+          _updated_from_api: 0,
+          _loc: "0000".to_string(),
+          _config_dir: get_app_dir(AppDataType::UserConfig, &APP_INFO, "").unwrap(),
+          _data_dir: data_dir.clone(),
+          _log_file_name: [data_dir.clone(), PathBuf::from("log.log")]
+            .iter()
+            .collect(),
+          _cache_file_name: [data_dir, PathBuf::from("cache.json")]
+            .iter()
+            .collect(),
+        },
+      };
+      CTX = mem::transmute(Box::new(context));
+      let ctx = &mut *CTX;
+      let _result = ctx.init(); // (*CTX).init(); does the same
+      ctx._cfg._is_intialiazed = true;
+    });
+    (*CTX)._use_count += 1;
+    &mut *CTX // return mutable reference to the object
+  }
 }
--- rust/src/context/datahandler.rs
+++ rust/src/context/datahandler.rs
@@ -2,161 +2,159 @@ use serde::Serialize;
 use std::collections::HashMap;
 
 lazy_static! {
-    static ref DAYTIME_CODES: HashMap<&'static str, char> = [
-        ("clear-day", 'a'),
-        ("clear-night", 'a'),
-        ("rain", 'g'),
-        ("snow", 'o'),
-        ("sleet", 'x'),
-        ("wind", '9'),
-        ("fog", '7'),
-        ("cloudy", 'e'),
-        ("partly-cloudy-day", 'c'),
-        ("partly-cloudy-night", 'C')
-    ]
-    .iter()
-    .copied()
-    .collect();
-    static ref NIGHTTIME_CODES: HashMap<&'static str, char> = [
-        ("clear-day", 'A'),
-        ("clear-night", 'A'),
-        ("rain", 'G'),
-        ("snow", 'O'),
-        ("sleet", 'x'),
-        ("wind", '9'),
-        ("fog", '7'),
-        ("cloudy", 'f'),
-        ("partly-cloudy-day", 'C'),
-        ("partly-cloudy-night", 'C')
-    ]
-    .iter()
-    .copied()
-    .collect();
+  static ref DAYTIME_CODES: HashMap<&'static str, char> = [
+    ("clear-day", 'a'),
+    ("clear-night", 'a'),
+    ("rain", 'g'),
+    ("snow", 'o'),
+    ("sleet", 'x'),
+    ("wind", '9'),
+    ("fog", '7'),
+    ("cloudy", 'e'),
+    ("partly-cloudy-day", 'c'),
+    ("partly-cloudy-night", 'C')
+  ]
+  .iter()
+  .copied()
+  .collect();
+  static ref NIGHTTIME_CODES: HashMap<&'static str, char> = [
+    ("clear-day", 'A'),
+    ("clear-night", 'A'),
+    ("rain", 'G'),
+    ("snow", 'O'),
+    ("sleet", 'x'),
+    ("wind", '9'),
+    ("fog", '7'),
+    ("cloudy", 'f'),
+    ("partly-cloudy-day", 'C'),
+    ("partly-cloudy-night", 'C')
+  ]
+  .iter()
+  .copied()
+  .collect();
 }
 
 #[derive(Serialize)]
 pub struct DataHandler {
-    wind_directions: [&'static str; 16],
-    pub history: bool,
-    pub tempunit: char,
-    pub cached: bool,
-    pub visunit: char,
-    pub windunit: char,
-    pub pressunit: char,
-    pub running: bool,
+  wind_directions: [&'static str; 16],
+  pub history: bool,
+  pub tempunit: char,
+  pub cached: bool,
+  pub visunit: char,
+  pub windunit: char,
+  pub pressunit: char,
+  pub running: bool,
 }
 
 impl DataHandler {
-    pub fn new() -> Self {
-        Self {
-            history: true,
-            tempunit: 'C',
-            cached: false,
-            visunit: 'k',
-            windunit: 'm',
-            pressunit: 'h',
-            running: false,
-            wind_directions: [
-                "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W",
-                "WNW", "NW", "NNW",
-            ],
-        }
+  pub fn new() -> Self {
+    Self {
+      history: true,
+      tempunit: 'C',
+      cached: false,
+      visunit: 'k',
+      windunit: 'm',
+      pressunit: 'h',
+      running: false,
+      wind_directions: [
+        "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW",
+        "NW", "NNW",
+      ],
     }
-    /// Convert wind bearing in degrees to a
-    /// returns a tuple (speed, unit)
-    ///
-    /// [`convert_wind speed`]: #method.convert_wind speed
-    ///
-    /// # Examples
-    /// assert_eq!(deg_to_bearing(45), "NE")
-    /// ```
-    pub fn deg_to_bearing(&self, mut wind_direction: i64) -> &'static str {
-        wind_direction = if wind_direction < 0 || wind_direction > 360 {
-            0
-        } else {
-            wind_direction
-        };
-        let val = (wind_direction as f64 / 22.5) + 0.5;
-        self.wind_directions[(val as usize) % 16]
-    }
+  }
+  /// Convert wind bearing in degrees to a
+  /// returns a tuple (speed, unit)
+  ///
+  /// [`convert_wind speed`]: #method.convert_wind speed
+  ///
+  /// # Examples
+  /// assert_eq!(deg_to_bearing(45), "NE")
+  /// ```
+  pub fn deg_to_bearing(&self, mut wind_direction: i64) -> &'static str {
+    wind_direction = if !(0..=360).contains(&wind_direction) {
+      0
+    } else {
+      wind_direction
+    };
+    let val = (wind_direction as f64 / 22.5) + 0.5;
+    self.wind_directions[(val as usize) % 16]
+  }
 
-    #[inline]
-    pub fn convert_temperature(&self, temp: f64, _unit: Option<char>) -> (f64, &'static str) {
-        let unit = _unit.unwrap_or('C');
-        // return a tuple, (temp, unit)
-        (
-            if unit == 'F' {
-                (temp * (9 / 5) as f64) + 32.0
-            } else {
-                temp
-            },
-            if unit == 'C' { "°C" } else { "°F" },
-        )
-    }
+  #[inline]
+  pub fn convert_temperature(&self, temp: f64, _unit: Option<char>) -> (f64, &'static str) {
+    let unit = _unit.unwrap_or('C');
+    // return a tuple, (temp, unit)
+    (
+      if unit == 'F' {
+        (temp * (9 / 5) as f64) + 32.0
+      } else {
+        temp
+      },
+      if unit == 'C' { "°C" } else { "°F" },
+    )
+  }
 
-    /// Convert visibility
-    /// returns a tuple (vis, unit)
-    ///
-    /// [`convert_vis`]: #method.convert_vis
-    ///
-    /// ```
-    #[inline]
-    pub fn convert_vis(&self, vis: f64, _unit: Option<char>) -> (f64, &'static str) {
-        let unit = _unit.unwrap_or('k');
-        (
-            if unit == 'm' { vis / 1.609 } else { vis },
-            if unit == 'm' { "miles" } else { "km" },
-        )
-    }
+  /// Convert visibility
+  /// returns a tuple (vis, unit)
+  ///
+  /// [`convert_vis`]: #method.convert_vis
+  ///
+  /// ```
+  #[inline]
+  pub fn convert_vis(&self, vis: f64, _unit: Option<char>) -> (f64, &'static str) {
+    let unit = _unit.unwrap_or('k');
+    (
+      if unit == 'm' { vis / 1.609 } else { vis },
+      if unit == 'm' { "miles" } else { "km" },
+    )
+  }
 
-    pub fn get_condition(&self, _cond: &str, _night: bool) -> char {
-        return if _night {
-            if NIGHTTIME_CODES.contains_key(_cond) {
-                *NIGHTTIME_CODES.get(_cond).unwrap()
-            } else {
-                'c'
-            }
-        } else {
-            if DAYTIME_CODES.contains_key(_cond) {
-                *DAYTIME_CODES.get(_cond).unwrap()
-            } else {
-                'c'
-            }
-        };
-    }
-    /// Convert wind speed
-    /// returns a tuple (speed, unit)
-    ///
-    /// [`convert_windspeed`]: #method.convert_windspeed
-    ///
-    /// ```
-    pub fn convert_windspeed(&self, speed: f64, _unit: Option<char>) -> (f64, &'static str) {
-        let unit = if _unit == None {
-            self.windunit
-        } else {
-            _unit.unwrap()
-        };
+  pub fn get_condition(&self, _cond: &str, _night: bool) -> char {
+    return if _night {
+      if NIGHTTIME_CODES.contains_key(_cond) {
+        *NIGHTTIME_CODES.get(_cond).unwrap()
+      } else {
+        'c'
+      }
+    } else if DAYTIME_CODES.contains_key(_cond) {
+      *DAYTIME_CODES.get(_cond).unwrap()
+    } else {
+      'c'
+    };
+  }
+  /// Convert wind speed
+  /// returns a tuple (speed, unit)
+  ///
+  /// [`convert_windspeed`]: #method.convert_windspeed
+  ///
+  /// ```
+  pub fn convert_windspeed(&self, speed: f64, _unit: Option<char>) -> (f64, &'static str) {
+    let unit = if let Some(..) = _unit {
+      self.windunit
+    } else {
+      _unit.unwrap()
+    };
 
-        return match unit {
-            'k' => (speed * 3.6, "km/h"),    // km/h
-            'm' => (speed * 2.237, "mph"),   // mph
-            'n' => (speed * 1.944, "knots"), // knots
-            _ => (speed, "m/s"),
-        };
+    match unit {
+      'k' => (speed * 3.6, "km/h"),    // km/h
+      'm' => (speed * 2.237, "mph"),   // mph
+      'n' => (speed * 1.944, "knots"), // knots
+      _ => (speed, "m/s"),
     }
+  }
 
-    // hPa > InHg
-    #[inline]
-    pub fn convert_pressure(&self, _hpa: f64, _unit: Option<char>) -> f64 {
-        let unit = if _unit == None {
-            self.pressunit
-        } else {
-            _unit.unwrap()
-        };
-        return if unit == 'i' {
-            _hpa / 33.863886666667
-        } else {
-            _hpa
-        };
+  // hPa > InHg
+  #[inline]
+  pub fn convert_pressure(&self, _hpa: f64, _unit: Option<char>) -> f64 {
+    let unit = if let Some(..) = _unit {
+      self.pressunit
+    } else {
+      _unit.unwrap()
+    };
+    if unit == 'i' {
+      _hpa / 33.863886666667
+    } else {
+      _hpa
     }
+  }
 }
